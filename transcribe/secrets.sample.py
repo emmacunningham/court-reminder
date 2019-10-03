d3bc0fdf4e6b29d09fce55fa57e6be5da5d35502
emma cunningham <emma@innovationlawlab.org>
2019-10-02 22:05:02 -0700

    Update transcribe/secrets.sample.py with explanation of triple quotes

--- transcribe/secrets.sample.py
+++ transcribe/secrets.sample.py
@@ -1,3 +1,7 @@
-bing_speech_api_key="bing speech api key"
-google_credentials_json = '{"project_id": "..."}'
-google_preferred_phrases = ['list of phrases', ] # https://cloud.google.com/speech/docs/basics#phrase-hints
+bing_speech_api_key = "bing speech api key"
+# The actual credential string is massive, so you'll want to use triple quotes here so you can span multiple lines
+google_credentials_json = '''
+    {"project_id": "..."}
+    '''
+google_preferred_phrases = [
+    'list of phrases', ]  # https://cloud.google.com/speech/docs/basics#phrase-hints
