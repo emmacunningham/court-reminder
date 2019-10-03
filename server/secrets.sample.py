fb2092de5874b150711c5212984716163a729d9d
emma cunningham <emma@innovationlawlab.org>
2019-10-03 15:23:14 -0700

    Add note about secret_key

--- server/secrets.sample.py
+++ server/secrets.sample.py
@@ -2,4 +2,4 @@
 # We can fill these in with whatever we want
 BASIC_AUTH_USERNAME = ''
 BASIC_AUTH_PASSWORD = ''
-SECRET_KEY = ''
+SECRET_KEY = ''  # Cannot leave empty or server will error; can populate with dummy text for local dev
