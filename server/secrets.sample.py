2d6a1c7f9f0c78353ec443f815551e1a37e30c3a
emma cunningham <emma@innovationlawlab.org>
2019-10-03 09:21:18 -0700

    Add comments to server/secrets.sample.py to explain variables defined

--- server/secrets.sample.py
+++ server/secrets.sample.py
@@ -1,3 +1,5 @@
+# Not dependent on anything from deployment, rather these credentials are used by Flask for basic authentication
+# We can fill these in with whatever we want
 BASIC_AUTH_USERNAME = ''
 BASIC_AUTH_PASSWORD = ''
 SECRET_KEY = ''
