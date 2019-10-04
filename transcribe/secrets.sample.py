f636ce0e9a80c89cd5b873e5abeb0373685f1e59
emma cunningham <emma@innovationlawlab.org>
2019-10-03 17:16:08 -0700

    Add comment about json string formatting

--- transcribe/secrets.sample.py
+++ transcribe/secrets.sample.py
@@ -1,5 +1,6 @@
 bing_speech_api_key = "bing speech api key"
 # The actual credential string is massive, so you'll want to use triple quotes here so you can span multiple lines
+# Must double escape any \n characters; this is an awful way to store this anyway
 google_credentials_json = '''
     {"project_id": "..."}
     '''
